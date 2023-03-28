246e7cb0770cb942922a052e563faeec53994715
zv1ln4 <119396256+zv1ln4@users.noreply.github.com>
2023-03-28 08:48:33 +0300

    Pievienojām funkcijas

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-const plusins = document.querySelector
-('.js-plus')
+const plusins = document.querySelector('.js-plus');
+const inputins = document.querySelector('.js-input');
 
 console.log(plusins)
-console.log( inputins)
+console.log(inputins)
+
+const Add = (cipars1, cipars2) => {
+return cipars1 + cipars2
+}
+
+const Subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', () => {
+console.log("Mani nospieda")
+})
+
