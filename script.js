acce029165c07bc1943d2f02eac3d754a1bacb05
zv1ln4 <119396256+zv1ln4@users.noreply.github.com>
2023-04-11 10:06:53 +0300

    plus pogai pievienoja nepieciešamo

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const plusins = document.querySelector('.js-plus');
-const inputins = document.querySelector('.js-input');
-
-console.log(plusins)
-console.log(inputins)
+const multiply = document.querySelector('.js-multiply');
+const minus = document.querySelector('.js-minus');
+const divide = document.querySelector('.js-divide');
+const inputins1 = document.querySelector('.js-input1');
+const inputins2 = document.querySelector('.js-input2');
+const answer = document.querySelector('.js-result');
 
 const Add = (cipars1, cipars2) => {
-return cipars1 + cipars2
+    return cipars1 + cipars2
 }
 
 const Subtract = (cipars1, cipars2) => {
@@ -21,6 +23,8 @@ const Divide = (cipars1, cipars2) => {
 }
 
 plusins.addEventListener('click', () => {
-console.log("Mani nospieda")
-})
+    answer.textContent = Add(parseInt(inputins1.value),parseInt(inputins2.value))
 
+    inputins1.value = ""
+    inputins2.value = ""
+})
